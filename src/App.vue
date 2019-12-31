9b7748e0537c36e0094b7d5ef712091a02bb9516
陈志杰 <1051788989@qq.com>
2019-12-31 13:50:37 +0800

    初始化地图

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <olmap />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import olmap from './components/olmap.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    olmap
   }
 }
 </script>
 
 <style>
+*{padding:0; margin:0;}
+html,body{
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
